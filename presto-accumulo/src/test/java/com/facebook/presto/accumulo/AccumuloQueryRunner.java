e1ffed33f11421825b08091fe99e35a3f11631ab
Nishitha-Bhaskaran <nishithakbhaskaran@gmail.com>
2025-04-22 19:30:11 +0530

    Pass core-sire.xml and add env variable -Dhadoop.profile=3 for test

--- presto-accumulo/src/test/java/com/facebook/presto/accumulo/AccumuloQueryRunner.java
+++ presto-accumulo/src/test/java/com/facebook/presto/accumulo/AccumuloQueryRunner.java
@@ -39,6 +39,7 @@ import org.intellij.lang.annotations.Language;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.facebook.presto.accumulo.AccumuloErrorCode.MINI_ACCUMULO;
@@ -196,6 +197,9 @@ public final class AccumuloQueryRunner
         MiniAccumuloCluster accumulo = new MiniAccumuloCluster(macDir, MAC_PASSWORD);
         accumulo.getConfig().setDefaultMemory(512, MEGABYTE);
         setConfigClassPath(accumulo.getConfig());
+        Map<String, String> coresiteMap = new HashMap<>();
+        coresiteMap.put("fs.defaultFS", "file:///");
+        accumulo.getConfig().setSiteConfig(coresiteMap);
         accumulo.start();
 
         // Add shutdown hook to stop MAC and cleanup temporary files
